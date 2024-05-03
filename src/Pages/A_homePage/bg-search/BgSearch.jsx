a1172fc18db36d360b415fdc0d0edb558b6b64bd
Panchammmm <sardarpancham510510@gmail.com>
2024-05-03 12:53:34 +0530

    searchBar

--- src/Pages/A_homePage/bg-search/BgSearch.jsx
+++ src/Pages/A_homePage/bg-search/BgSearch.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import './bgsearch.css';
 
 import BG from '../../../assets/bg/bg-search.jpg';
+import { IoSearchCircle } from "react-icons/io5";
 
-export default function BgSearch (){
-    return(
+export default function BgSearch() {
+    return (
         <>
             <div className="bg-container">
                 <img src={BG} alt="bg"></img>
+
+                <h1>Anistora Exclusives</h1>
+
+                <div class="search-container">
+                    <input type="text" placeholder="Search"></input>
+                    <button type="submit"><IoSearchCircle className="search-icon" /></button>
+                </div>
             </div>
         </>
     )
